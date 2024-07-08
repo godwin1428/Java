dd3d2962392dc072acc7d3a2a3344e9b5b45a9c2
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-08 20:02:53 +0530

    Create String to Integer.java

---
+++ Easy Programs/String to Integer.java
@@ -0,0 +1,14 @@
+/* Write a program to convent the given string to integer? 
+Sample Input: 
+String: 1234
+Sample Output:
+Out put String: 1234 */
+
+class Main{
+    public static void main(String[] args) {
+        String a="1234";
+        int b=Integer.valueOf(a);
+        System.out.println("integer:"+b);
+    }
+}
+
