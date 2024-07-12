d664342912a51a4d55b44401402b774a7a438029
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-12 08:59:37 +0530

    Update 36. Composite no btw range.java

--- Easy Programs/36. Composite no btw range.java
+++ Easy Programs/36. Composite no btw range.java
@@ -1,1 +1,18 @@
-
+class SI {
+    public static void main(String[] args)
+    {   int a=12,b=19,count;
+        for(int i=a+1;i<19;i++)
+        {count=0;
+        for(int j=2;j<i;j++)
+        {
+          if(i%j==0)
+          {
+            count++;
+          }
+        }
+         if(count>0)
+         {System.out.print(i+" ");}
+        }
+        
+    }
+}
