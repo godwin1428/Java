5249eecc4f8e44a0c9f1970d99814d0a396644db
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-12 08:53:28 +0530

    Update 40. Perfect or Not.java

--- Easy Programs/40. Perfect or Not.java
+++ Easy Programs/40. Perfect or Not.java
@@ -1,1 +1,16 @@
-
+class SI {
+    public static void main(String[] args)
+    {   int n=28,sum=0;
+        for(int i=1;i<n;i++)
+        {
+          if(n%i==0)
+          {
+              sum=sum+i;
+          }
+        }
+        if(sum==n){
+         System.out.print("Perfect num");}
+        else{
+         System.out.print("Not a Perfect num");}
+    }
+}
