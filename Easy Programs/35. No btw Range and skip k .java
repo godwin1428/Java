1048adf4a95590952b3db8b1e26e1ec23588b6f1
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-12 08:45:20 +0530

    Create 35. No btw Range and skip k .java

---
+++ Easy Programs/35. No btw Range and skip k .java
@@ -0,0 +1,10 @@
+class SI {
+    public static void main(String[] args)
+    {   int m=50,n=100,k=7;
+        for(int i=m;i<=n;i=i+k)
+        {
+             System.out.print(i+" ");
+        }
+        
+    }
+}
