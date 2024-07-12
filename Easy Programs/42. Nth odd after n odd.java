ace8b9f69313868de1f09e03849f6e96832a28e5
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-12 20:20:52 +0530

    Create 42. Nth odd after n odd.java

---
+++ Easy Programs/42. Nth odd after n odd.java
@@ -0,0 +1,12 @@
+/*42.	Find the nth odd number after n odd number
+Sample Input: N : 7
+  output: 20 */
+
+
+public class Main {
+    public static void main(String[] args) {
+        int n=5;
+        int ans=((2*n)-1)+n;
+        System.out.println(ans);
+    }
+}
