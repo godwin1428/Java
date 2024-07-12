f5e91cd42bc5eeab9a2a915480f2423a727c8d6c
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-12 08:46:48 +0530

    Create 37. Factorial.java

---
+++ Easy Programs/37. Factorial.java
@@ -0,0 +1,10 @@
+class fact {
+    public static void main(String[] args)
+    {   int n=4,pr=1;
+        for(int i=1;i<=n;i++)
+        {
+            pr=pr*i;
+        }
+         System.out.print(pr);
+    }
+}
