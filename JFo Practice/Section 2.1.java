8ddd623f3e4a9c21e0086b05ea0b41f3879b203e
T Godwin Abilash <154417008+godwin1428@users.noreply.github.com>
2024-07-29 19:44:25 +0530

    Create Section 2.1.java

---
+++ JFo Practice/Section 2.1.java
@@ -0,0 +1,14 @@
+public class ProblemSet2_1 {
+    public static void main(String[] args) {
+        // Use 8 print statements to print a smiley face.
+        // Your art will rely on only a single character, besides space, such as X or #.
+        System.out.println("  XXXXX  ");
+        System.out.println(" X     X ");
+        System.out.println("X  X X  X");
+        System.out.println("X       X");
+        System.out.println("X  XXX  X");
+        System.out.println(" X     X ");
+        System.out.println("  XXXXX  ");
+        System.out.println("         ");
+    }
+}
